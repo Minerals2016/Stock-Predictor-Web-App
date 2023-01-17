b8857c04ba619904264b0ef86f5447ae06988b76
Minerals2016 <Minerals2016@users.noreply.github.com>
2023-01-16 22:55:29 -0500

    Messing with packages
    just uploading my changes that i made by messing around with configuring the required packages

--- Streamlit Web App/stockpredictornii.py
+++ Streamlit Web App/stockpredictornii.py
@@ -1,8 +1,28 @@
 import streamlit as st
 from datetime import date
-
 import yfinance as yf
 from plotly import graph_objs as go
+
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_style('whitegrid')
+plt.style.use("fivethirtyeight")
+
+# packages that were not added
+import math
+from sklearn.preprocessing import MinMaxScaler
+
+# more packages that were not added
+# from tensorflow.keras import Sequential
+# from tensorflow.keras.layers import Dense, LSTM
+
+# For reading stock data from yahoo
+# from pandas_datareader.data import DataReader
+
 
 START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
@@ -14,6 +34,10 @@ selected_stock = st.selectbox('Select dataset for prediction', stocks)
 
 n_years = st.slider('Years of prediction:', 1, 4)
 period = n_years * 365
+
+#from tensorflow import keras
+#reconstructed_model = keras.models.load_model("keras_model.h5s")
+
 
 @st.cache
 def load_data(ticker):
@@ -29,6 +53,7 @@ data_load_state.text('Loading data... done!')
 st.subheader('Raw data')
 st.write(data.tail())
 
+
 # Plot raw data
 def plot_raw_data():
     fig = go.Figure()
@@ -36,5 +61,6 @@ def plot_raw_data():
     fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
     fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
     st.plotly_chart(fig)
+
 
 plot_raw_data()
